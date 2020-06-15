e5b8df28992b6f1e73b57523d9f33e5b7133e67c
dhkho <dhkho@DESKTOP-MD1V7FM>
2020-06-15 11:29:31 -0700

    changing parameter order

--- src/main/java/SharedPhotosCRUD/GroupAlbum.java
+++ src/main/java/SharedPhotosCRUD/GroupAlbum.java
@@ -1,8 +1,8 @@
 package SharedPhotosCRUD;
 
 public class GroupAlbum {
-	private String groupName;
 	private String albumName;
+	private String groupName;
 	private String accountName;
 	
 	GroupAlbum(){
@@ -15,19 +15,18 @@ public class GroupAlbum {
 		this.setAccountName(accountName);
 	}
 	
-	public String getGroupName() {
-		return groupName;
-	}
-	public void setGroupName(String groupName) {
-		this.groupName = groupName;
-	}	
 	public String getAlbumName() {
 		return albumName;
 	}
 	public void setAlbumName(String accountName) {
 		this.albumName = accountName;
 	}
-
+	public String getGroupName() {
+		return groupName;
+	}
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}	
 	public String getAccountName() {
 		return accountName;
 	}
