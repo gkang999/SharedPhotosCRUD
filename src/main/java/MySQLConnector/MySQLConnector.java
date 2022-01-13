9fe424bc63c7fe38cb6c29b9cde8f749ce6c3c10
skang27 <skang27@ucsc.edu>
2022-01-12 19:20:56 -0800

    initial commit

--- src/main/java/MySQLConnector/MySQLConnector.java
+++ src/main/java/MySQLConnector/MySQLConnector.java
@@ -10,22 +10,23 @@ public class MySQLConnector {
 	
 	public void makeJDBCConnection() {
 		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			SysOLog.log("Congrats - Seems your MySQL JDBC Driver Registered!");
-		} catch (ClassNotFoundException e) {
-			SysOLog.log("Sorry, couldn't found JDBC driver. Make sure you have added JDBC Maven Dependency Correctly");
-			e.printStackTrace();
-			return;
-		}
+//		try {
+//			Class.forName("com.mysql.jdbc.Driver");
+//			SysOLog.log("Congrats - Seems your MySQL JDBC Driver Registered!");
+//		} catch (ClassNotFoundException e) {
+//			SysOLog.log("Sorry, couldn't found JDBC driver. Make sure you have added JDBC Maven Dependency Correctly");
+//			e.printStackTrace();
+//			return;
+//		}
  
 		try {
 			// DriverManager: The basic service for managing a set of JDBC drivers.
-			String jdbcConnString = "jdbc:mysql://127.0.0.1:3306/sharedPhotosConn?useSSL=false";
+			String jdbcConnString = "jdbc:mysql:///sharedphotosconn?useSSL=false&cloudSqlInstance=sharedphotos-337902:us-west2:sharedphotosdb"
+					+ "&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=spuser&password=sppass";
 			String jdbcUser = "root";
 			String jdbcPassword = "209146Mouse*";
 			
-			sharedPhotosConn = DriverManager.getConnection(jdbcConnString, jdbcUser, jdbcPassword);
+			sharedPhotosConn = DriverManager.getConnection(jdbcConnString);
 			if (sharedPhotosConn != null) {
 				SysOLog.log("Connection Successful! Enjoy. Now it's time to push data");
 			} else {
